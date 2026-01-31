beede30fa352c45dfc83087f7defcc9b360769d6
zanemcmorris <zamc8851@colorado.edu>
2026-01-31 13:09:54 -0700

    Replacing something dumb

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -15,6 +15,11 @@ FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 NUM_CORES=$(nproc)
+SYSROOT=$(aarch64-none-linux-gnu-gcc -print-sysroot)
+
+echo "Zane printing sysroot"
+echo ${SYSROOT}
+exit
 
 # SYSROOT=/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu
 # export PATH="/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin/:$PATH"echo "ROOT=${ROOT-<unset>}"
