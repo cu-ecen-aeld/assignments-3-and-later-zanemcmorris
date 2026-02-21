5548a1afc0df179e04a36e82b2e0fa13ab28f59a
zanemcmorris <zamc8851@colorado.edu>
2026-02-21 09:01:27 -0700

    Working multithreaded socketserver

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -7,13 +7,21 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
+#include <time.h>
 #include <errno.h>
 #include <syslog.h>
 #include <stdbool.h>
+#include <sys/stat.h>
+#include "queue.h"
+#include <pthread.h>
+#include <sys/time.h>
+#include <stdatomic.h>
 
-#define MAX_SOCK_CONNECTIONS (1)
+#define MAX_SOCK_CONNECTIONS (100)
 #define LOG_PATH ("/var/tmp/aesdsocketdata")
+#define SOCKET_PORT ("9000")
 #define RECV_BUFFER_LENGTH_BYTES (32)
+#define ITIMER_PERIOD_SEC (10)
 
 typedef struct sockaddr sockaddr_t;
 typedef struct addrinfo addrinfo_t;
@@ -22,19 +30,85 @@ typedef struct addrinfo addrinfo_t;
 addrinfo_t *addrinfo = NULL;
 int sockfd = -1;
 int logfd = -1;
-int clientfd = -1;
+// int clientfd = -1;
 volatile int endProgram = 0;
 bool runAsDaemon = false;
+pthread_mutex_t logMutex;
+timer_t intervalTimerID = 0;
+
+// SLIST.
+typedef struct slist_data_s slist_data_t;
+struct slist_data_s {
+    pthread_t thread;      
+    _Atomic bool isThreadComplete;
+    int clientfd;
+    SLIST_ENTRY(slist_data_s) entries;
+};
+
+SLIST_HEAD(threadListHead, slist_data_s) head;
 
 int cleanupProgram();
 
+/**
+ * @brief Signal Handler. Expects SIGINT and SIGTERM to end program.
+ */
 void handle_sigint(int sig) {
     syslog(LOG_DEBUG, "Caught signal, exiting");
 
-    // Perform cleanup here
-    endProgram = 1;
+    if(sig == SIGINT || sig == SIGTERM){
+        endProgram = 1;
+    } else {
+        syslog(LOG_DEBUG, "Caught unknown signal. Exiting.");
+    }
+
+    endProgram = 1; // Set flag to cleanup and end the program
 }
 
+/**
+ * @brief POSIX Interval timer callback thread. Logs timestamps to LOG_PATH
+ */
+static void timerCallback()
+{
+    // TODO: Print something to log
+
+
+    return;
+}
+
+/**
+ * @brief Create and start interval timer for logging service
+ */
+int startIntervalLoggingTimer()
+{
+    struct sigevent sev = {0};
+    sev.sigev_notify = SIGEV_THREAD;
+    sev.sigev_notify_function = timerCallback;
+    sev.sigev_notify_attributes = NULL;
+
+    int rc = timer_create(CLOCK_MONOTONIC, &sev, &intervalTimerID);
+    if(rc == -1){
+        perror("timer create failed");
+        return -1;
+    }
+
+    struct itimerspec timerData = {0};
+    timerData.it_interval.tv_sec = ITIMER_PERIOD_SEC;
+    timerData.it_value.tv_sec = ITIMER_PERIOD_SEC;
+
+    rc = timer_settime(intervalTimerID, 0, &timerData, NULL);
+    if(rc == -1){
+        perror("timer_settime failed");
+        return -1;
+    }
+
+    printf("itimer should be set up\n");
+
+    return 0;
+}
+
+/**
+ * @brief Frees allocated memory and cleans up logfile
+ */
 int cleanupProgram()
 {
     if(addrinfo != NULL){
@@ -52,11 +126,6 @@ int cleanupProgram()
         logfd = -1;
     }
 
-    if(clientfd != -1){
-        close(clientfd);
-        clientfd = -1;
-    }
-
     int rc;
     rc = access(LOG_PATH, F_OK);
     if(rc == 0){
@@ -69,13 +138,16 @@ int cleanupProgram()
     printf("Cleaned!\n");
 }
 
-void printClientNameConnected(struct sockaddr *clientaddr){
+/**
+ * @brief Helper function to print the connecting client's address and port.
+ */
+void printClientNameConnected(struct sockaddr *clientaddr, size_t clientAddrSize){
     char host[NI_MAXHOST];
     char service[NI_MAXSERV];
     int rc;
 
     rc = getnameinfo((struct sockaddr *) clientaddr,
-                        sizeof(*clientaddr),
+                        clientAddrSize,
                         host,
                         sizeof(host),
                         service,
@@ -89,6 +161,10 @@ void printClientNameConnected(struct sockaddr *clientaddr){
     }
 }
 
+/**
+ * @brief Helper function to show the disconnected client's address and port 
+ * 
+ */
 void printClientNameDisconnected(struct sockaddr *clientaddr){
     char host[NI_MAXHOST];
     char service[NI_MAXSERV];
@@ -110,6 +186,10 @@ void printClientNameDisconnected(struct sockaddr *clientaddr){
     }
 }
 
+/**
+ * @brief Opens a stream-style socket on port specified
+ * @return 0 on success, -1 on failure.
+ */
 int openSocket(const char* port){
     sockfd = socket(AF_INET, SOCK_STREAM, 0);    
     if(sockfd == -1){
@@ -167,6 +247,11 @@ int openSocket(const char* port){
 
 }
 
+/**
+ * @brief Sends the full log of what was received on the socket to newfd
+ * @details NOT thread-safe!! Use mutex around me for logdata!
+ * @return Returns 0 on success, -1 on failure.
+ */
 int sendFullLog(int newfd)
 {
     int fd = open(LOG_PATH, O_RDONLY);
@@ -175,13 +260,31 @@ int sendFullLog(int newfd)
         return -1;
     }
 
-    char buf[4096];
+    struct stat st;
+    int rc = stat(LOG_PATH, &st);
+    if(rc == -1){
+        perror("Stat failed");
+        return -1;
+    }
+
+    int fsize = st.st_size;
+    printf("fsize: %d\n", fsize);
+
+    char *buf = malloc(sizeof(char) * fsize);
+    if(buf == NULL)
+    {
+        // Malloc failed
+        syslog(LOG_DEBUG, "Malloc failed in sendFullLog");
+        return -1;
+    }
+
     while (1) {
-        ssize_t n = read(fd, buf, sizeof(buf));
-        if (n == 0) break;              // EOF
+        ssize_t n = read(fd, buf, fsize);
+        if (n == 0) break;
         if (n < 0) {
             perror("read log");
             close(newfd);
+            free(buf);
             return -1;
         }
 
@@ -192,16 +295,114 @@ int sendFullLog(int newfd)
             if (m < 0) {
                 perror("send");
                 close(newfd);
+                free(buf);
                 return -1;
             }
             sent += m;
         }
     }
 
+    free(buf);
     close(fd);
     return 0;
 }
 
+/**
+ * @brief Pthread recieving thread for multi-threaded server. Spawned on each new connection
+ * @arg Pointer to linkedlist node containing thread-specific information  
+ * @return None
+ */
+void* repsondingThread(void* arg)
+{
+    slist_data_t *myNodeData = (slist_data_t*) (arg);
+    int clientFD = myNodeData->clientfd;
+
+    // Set up to recv from client
+    size_t bufferCapacity = RECV_BUFFER_LENGTH_BYTES;
+    uint8_t *buffer = (uint8_t*) malloc(bufferCapacity);
+    memset(buffer, 0, bufferCapacity);
+    int totalBytesRecvd = 0; // Reset num bytes received for this message
+    bool failedToRead = false;
+
+    // Receive all the bytes now
+    while(1)
+    {
+        // If received bytes on last ittr was at buffer capacity, then double capacity and keep going
+        if(totalBytesRecvd == bufferCapacity){
+            bufferCapacity *= 2;
+            uint8_t *temp = realloc(buffer, bufferCapacity);
+            if(temp == NULL){
+                free(buffer);
+                // toss this message and go next if malloc failed 
+                failedToRead = true;
+                printf("Failed to malloc large enough buffer.\n");
+                break;
+            }
+
+            buffer = temp;
+            printf("Doubled buffer size. Now %ld bytes\n", bufferCapacity);
+        }
+
+        // Recieve n bytes from the client
+        int n = recv(clientFD, buffer + totalBytesRecvd, bufferCapacity - totalBytesRecvd, 0);
+        if(n < 0){
+            // Error on recv
+            free(buffer);
+            buffer = NULL;
+            perror("recv failed");
+            return NULL;
+        }
+
+        if(n == 0){
+            // We read all the data out of the socket
+            break;
+        }
+
+        totalBytesRecvd += n;
+        printf("Just read %d bytes, making total of %d\n", n, totalBytesRecvd);
+
+        // Only stop loop if the final char is a newline
+        if(buffer[totalBytesRecvd-1] == '\n'){
+            break;
+        }
+    }
+
+    // Trap for failed to read to hit the cleanup and return step at the end of function
+    if(!failedToRead){
+        // If we read completely, write message to the log, free the buffer and echo back log
+        buffer[totalBytesRecvd] = 0;
+        printf("new buffer: %s", buffer);
+
+        pthread_mutex_lock(&logMutex);
+        write(logfd, buffer, totalBytesRecvd); // Protext the log write
+        pthread_mutex_unlock(&logMutex);
+
+        if(buffer != NULL)
+        {
+            free(buffer);
+            buffer = NULL;
+        }   
+
+        pthread_mutex_lock(&logMutex);
+        sendFullLog(clientFD); // Protect the log read
+        pthread_mutex_unlock(&logMutex);
+    }
+
+    shutdown(clientFD, SHUT_RDWR);
+    close(clientFD);
+    clientFD = -1;
+    atomic_store(&myNodeData->isThreadComplete, true);
+    printf("Thread cleaned up!\n");
+
+    return NULL;
+
+}
+
+/**
+ * @brief Main loop that starts listening on the opened socket and dispatches new threads upon connection
+ *          Only returns via upon receiving SIGINT or SIGTERM.
+ * @return Returns 1 on successful cleanup, -1 on failure. Does not return without catching a signal to do so.
+ */
 int listenLoop()
 {
     int rc = 0;
@@ -213,24 +414,20 @@ int listenLoop()
         return -1;
     }
 
-
     printf("starting to listen...\n");
     rc = listen(sockfd, MAX_SOCK_CONNECTIONS);
     if(rc == -1){
         perror("listen failed");
     }
 
-    uint32_t bufferCapacity = RECV_BUFFER_LENGTH_BYTES;
-    uint8_t *buffer = NULL;
-    uint8_t *tmp;
-    size_t totalBytesRecvd = 0;
-    bool failedToRead = false;
+    int clientAddrSize = sizeof(clientaddr);
+    SLIST_INIT(&head); // Create LL for threadss
 
     do
     {
-        int sockaddrsize = sizeof(sockaddr_t);
         // accept is a blocking call. Execution will wait here for a connection
-        clientfd = accept(sockfd, (sockaddr_t*) &clientaddr, &sockaddrsize);
+        printf("Main thread accepting new connections...\n");
+        int clientfd = accept(sockfd, (sockaddr_t*) &clientaddr, &clientAddrSize);
         if(clientfd == -1){
             if(errno == EINTR && endProgram){
                 break;
@@ -238,74 +435,48 @@ int listenLoop()
                 perror("accept failed");
             }
         }
-        printClientNameConnected((sockaddr_t *) &clientaddr);
+        printClientNameConnected((sockaddr_t *) &clientaddr, clientAddrSize);
 
-        // Set up to recv from client
-        bufferCapacity = RECV_BUFFER_LENGTH_BYTES;
-        buffer = (uint8_t*) malloc(bufferCapacity);
-        memset(buffer, bufferCapacity, 0);
-        totalBytesRecvd = 0; // Reset num bytes received for this message
+        // Create new LinkedList node for the new pthread and give it the clientfd to act on
+        slist_data_t *newNode = NULL;
+        newNode = malloc(sizeof(slist_data_t));
+        memset(newNode, 0, sizeof(*newNode));
+        newNode->clientfd = clientfd;
+        SLIST_INSERT_HEAD(&head, newNode, entries);
 
-        // Receive all the bytes now
-        while(1)
-        {
-            if(totalBytesRecvd == bufferCapacity){
-                bufferCapacity *= 2;
-                uint8_t *temp = realloc(buffer, bufferCapacity);
-                if(temp == NULL){
-                    free(buffer);
-                    // toss this message and go next...
-                    failedToRead = true;
-                    printf("Failed to malloc large enough buffer.\n");
-                    break;
-                }
+        // Create thread to respond to server request
+        pthread_create(&newNode->thread, NULL, repsondingThread, newNode);
+        if(newNode == NULL){
+            printf("Failed to make newNode in listenloop\n");
+            return -1;
+        }
 
-                buffer = temp;
-                printf("Doubled buffer size. Now %d bytes\n", bufferCapacity);
-            }
+        // Look to clean up any threads that are complete
+        slist_data_t* dataptr = NULL, *tmp = NULL;
+        SLIST_FOREACH_SAFE(dataptr, &head, entries, tmp){
+            if(atomic_load(&dataptr->isThreadComplete)){
+                SLIST_REMOVE(&head, dataptr, slist_data_s, entries);
+                pthread_join(dataptr->thread, NULL);
+                free(dataptr);
 
-            int n = recv(clientfd, buffer + totalBytesRecvd, bufferCapacity - totalBytesRecvd, 0);
-            if(n < 0){
-                // Error on recv
-                free(buffer);
-                buffer = NULL;
-                perror("recv");
-                return -1;
-            }
-
-            if(n == 0){
-                // We read all the data out of the socket
-                break;
-            }
-
-            totalBytesRecvd += n;
-            printf("Just read %d bytes, making total of %ld\n", n, totalBytesRecvd);
-            if(buffer[totalBytesRecvd-1] == '\n'){
-                break;
             }
         }
 
-        if(!failedToRead){
-            // If we read completely, write message to the log, free the buffer and echo back log
-            buffer[totalBytesRecvd] = 0;
-            printf("new buffer: %s", buffer);
-            write(logfd, buffer, totalBytesRecvd);
-            if(buffer != NULL)
-            {
-                free(buffer);
-                buffer = NULL;
-            }   
-            sendFullLog(clientfd);
-        }
+    }while(!endProgram);
 
-        printClientNameDisconnected((sockaddr_t *) &clientaddr);
-        shutdown(clientfd, SHUT_RDWR);
-        close(clientfd);
-        clientfd = -1;
+    // Program is ending. Clean up whole LL
+    slist_data_t* dataptr = NULL, *tmp = NULL;
+    SLIST_FOREACH_SAFE(dataptr, &head, entries, tmp){
+        SLIST_REMOVE(&head, dataptr, slist_data_s, entries);
+        pthread_join(dataptr->thread, NULL);
+        free(dataptr);
+    }
 
-    }while(!endProgram);    
 }
 
+/**
+ * @brief entry point for program
+ */
 int main(int argc, char ** argv){
 
     if(argc == 2)
@@ -326,11 +497,20 @@ int main(int argc, char ** argv){
     sigaction(SIGTERM, &sa, NULL);
 
     // open a socket on port 9000
-    int rc = openSocket("9000");
+    int rc = openSocket(SOCKET_PORT);
+
+    // Now init mutex to procet the log data
+    rc = pthread_mutex_init(&logMutex, NULL);
+    if(rc == -1){
+        perror("mutex create failed");
+    }
+
     
+    startIntervalLoggingTimer();
+
     // Listen for and accept new connection
     if(rc == 0)
-        listenLoop();
+        rc = listenLoop();
 
     cleanupProgram();
     return rc;
