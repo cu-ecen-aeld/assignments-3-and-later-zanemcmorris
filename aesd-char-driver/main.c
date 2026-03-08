3335201b1369fcfa68513475971d210956315dd2
zanemcmorris <zamc8851@colorado.edu>
2026-03-08 17:55:10 -0600

    updating makefile for chardriver

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -18,7 +18,6 @@
 #include <linux/cdev.h>
 #include <linux/fs.h> // file_operations
 #include "aesdchar.h"
-#include "aesd-circular-buffer.h"
 int aesd_major = 0; // use dynamic major
 int aesd_minor = 0;
 
