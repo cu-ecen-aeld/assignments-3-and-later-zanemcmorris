4ad0f3aeb8800e2273ee84b394a726131e57a997
zanemcmorris <zamc8851@colorado.edu>
2026-03-13 16:49:41 -0600

    removing ioctl

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -303,31 +303,31 @@ loff_t aesd_llseek(struct file* filp, loff_t offset, int whence){
 
 long aesd_unlockedioctl(struct file* filp, unsigned int cmd, unsigned long arg)
 {
-    PDEBUG("unlocked_ioctl with cmd %d and arg %d", cmd, arg);
+    // PDEBUG("unlocked_ioctl with cmd %d and arg %d", cmd, arg);
 
-    struct aesd_dev *dev;
-    dev = filp->private_data;
+    // struct aesd_dev *dev;
+    // dev = filp->private_data;
 
     
-    switch(cmd){
+    // switch(cmd){
 
-        case AESDCHAR_IOCSEEKTO:
-            struct aesd_seekto seekData = (struct aesd_seekto) arg; // Recast arg as seekdata
-            PDEBUG("Parse seekData as write_cmd:%d and offset:%d", seekData.write_cmd, seekData.write_cmd_offset);
+    //     case AESDCHAR_IOCSEEKTO:
+    //         struct aesd_seekto seekData = (struct aesd_seekto) arg; // Recast arg as seekdata
+    //         PDEBUG("Parse seekData as write_cmd:%d and offset:%d", seekData.write_cmd, seekData.write_cmd_offset);
 
-            mutex_lock(&dev->deviceMutex);
+    //         mutex_lock(&dev->deviceMutex);
 
-            int circBufferEntryIndex = dev->circBufferPtr->out_offs;
-            circBufferEntryIndex = (circBufferEntryIndex + seekData.write_cmd)\
-             % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; // 
+    //         int circBufferEntryIndex = dev->circBufferPtr->out_offs;
+    //         circBufferEntryIndex = (circBufferEntryIndex + seekData.write_cmd)\
+    //          % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; // 
             
-            mutex_unlock(&dev->deviceMutex);
+    //         mutex_unlock(&dev->deviceMutex);
 
 
-            break;
-        default:
-            return -EINVAL;
-    }
+    //         break;
+    //     default:
+    //         return -EINVAL;
+    // }
 
     return 0;
 
